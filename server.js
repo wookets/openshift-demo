e82330a78ae30e94236ce375fc049321af41eecc
Sean Wesenberg <wookets@gmail.com>
2017-05-20 08:09:32 -0500

    Added a /healthz check

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-const PORT = process.env.NODE_ENV === 'production' ? 8080 : 3000;
+const pkg = require('./package.json');
 const express = require('express')
 const app = express()
 
@@ -7,6 +7,11 @@ app.get('/', function (req, res) {
   res.send('Meow Yarn!')
 })
 
+app.get('/healthz', function (req, res) {
+  res.send({version: pkg.version})
+})
+
+const PORT = process.env.NODE_ENV === 'production' ? 8080 : 3000;
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`)
 })
