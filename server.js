a6fe8bcc53885a3f2731a55e513ff4cf659582ab
Sean Wesenberg <wookets@gmail.com>
2017-05-19 20:37:53 -0500

    Test rolling deploys

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Meow!')
+  res.send('Meow Yarnball!')
 })
 
 app.listen(PORT, function () {
