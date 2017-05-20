a299c5cda06b1c7c3459e04e2943c3f65f78f69c
Sean Wesenberg <wookets@gmail.com>
2017-05-19 19:59:20 -0500

    Enable port pick for prod env

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+const PORT = process.env.NODE_ENV === 'production' ? 8080 : 3000;
 const express = require('express')
 const app = express()
 
@@ -5,6 +7,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`)
 })
