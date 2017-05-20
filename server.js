747c8f1fbee19e2f9379ab08f64e1a338c42b40c
Sean Wesenberg <wookets@gmail.com>
2017-05-19 21:57:57 -0500

    openshift test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Meow Yarnball!')
+  res.send('Meow Yarn!')
 })
 
 app.listen(PORT, function () {
